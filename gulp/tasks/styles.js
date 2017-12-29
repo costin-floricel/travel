8aec0e8ad12d74b9e27fdabca2b01441a9aecc2b
Costin Floricel <costin_floricel@hotmail.com>
2017-12-29 12:05:50 +0100

    Completed desktop and mobile header styles.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,18 @@
-var gulp = require ('gulp');
-var postcss = require ('gulp-postcss');
-var autoprefixer = require ('autoprefixer');
-var cssvars = require ('postcss-simple-vars');
-var nested = require ('postcss-nested');
-var cssImport = require('postcss-import');
-var mixins = require ('postcss-mixins');
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
-
-gulp.task('styles' , function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	 .pipe(postcss([cssImport,mixins, nested, cssvars, autoprefixer]))
-	 .on('error', function(errorInfo){
-	 	console.log(errorInfo.toString());
-	 	this.emit('end');
-	 })
-	 .pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,6 +18,10 @@ gulp.task('watch' , function(){
 	watch('./app/assets/styles/**/*.css' , function(){
 		gulp.start('cssInject');
 	});
+
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
+	})
 });
 
 //The css inject task
@@ -25,3 +29,7 @@ gulp.task('cssInject',['styles'], function(){
 	return gulp.src('./app/temp/styles/styles.css')
 		.pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh',['scripts'], function(){
+	browserSync.reload();
+})
