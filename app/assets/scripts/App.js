19709ae1e6172ca5c4e879455993b3afa70d9670
Costin Floricel <costin_floricel@hotmail.com>
2018-01-22 19:58:40 +0100

    Finished the sticky header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
@@ -8,3 +9,4 @@ var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
